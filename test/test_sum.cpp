9da56b510f9c7409bb8e2a5dc5545456583a3565
Jae-hyeon Park <jhyeon@gmail.com>
2014-07-11 18:09:12 +0200

    test/test_sum.cpp: test looping sum

--- test/test_sum.cpp
+++ test/test_sum.cpp
@@ -37,13 +37,29 @@ BOOST_AUTO_TEST_CASE(test_sum)
     double c = 6;
     BOOST_CHECK_CLOSE_FRACTION(sqrt(SUM(i, 1, 1000, c/i/i)), 3.14, 1e-3);
     BOOST_CHECK_EQUAL(SUM(char, i, -10, 0, i), -55);
+}
+
+BOOST_AUTO_TEST_CASE(test_usum)
+{
+    BOOST_CHECK_EQUAL((usum<size_t, 0, 10>([](size_t i){ return i; })), 55);
+    BOOST_CHECK_EQUAL((usum<char, -10, 0>([](char i){ return i; })), -55);
+
+    double c = 6;
+    BOOST_CHECK_CLOSE_FRACTION(sqrt(USUM(i, 1, 1000, c/i/i)), 3.14, 1e-3);
+    BOOST_CHECK_EQUAL(USUM(char, i, -10, 0, i), -55);
 
     // monitor evaluation order
-    BOOST_CHECK_EQUAL(SUM(i,1,100, (cout << "i=" << i << "\n", pow(-1,i))), 0);
+    BOOST_CHECK_EQUAL(USUM(i,1,100,(cout << "i=" << i << "\n", pow(-1,i))), 0);
 }
 
 BOOST_AUTO_TEST_CASE(test_multi_sum)
 {
     BOOST_CHECK_EQUAL(SUM(i, 1, 3, SUM(j, 1, 5, (i+7)*j)), 405);
     BOOST_CHECK_EQUAL(SUM(char, i, -4, 0, SUM(int, j, 1, 9, i+j)), 135);
+}
+
+BOOST_AUTO_TEST_CASE(test_mixed_sum)
+{
+    BOOST_CHECK_EQUAL(SUM(i, 1, 3, USUM(j, 1, 5, (i+7)*j)), 405);
+    BOOST_CHECK_EQUAL(SUM(char, i, -4, 0, USUM(int, j, 1, 9, i+j)), 135);
 }
