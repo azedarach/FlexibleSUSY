4cf74d4b8d22abe83d683b12e361de89827e0e1d
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2017-11-08 11:30:31 +0100

    adding some more comments

--- meta/SelfEnergies2L.m
+++ meta/SelfEnergies2L.m
@@ -12,7 +12,7 @@ AppendIndex[Susyno`LieGroups`conj[p_], idx_] := Susyno`LieGroups`conj[AppendInde
 AppendIndex[p_, idx_] := p[{idx}];
 
 DistChir[{}, {}] := 1;
-DistChir[{0, rest1___}, {c_, rest2___}] := c DistributeChiralities[{rest1}, {rest2}];
+DistChir[{0, rest1___}, {c_, rest2___}] := c     DistributeChiralities[{rest1}, {rest2}];
 DistChir[{L, rest1___}, {c_, rest2___}] := c[PL] DistributeChiralities[{rest1}, {rest2}];
 DistChir[{R, rest1___}, {c_, rest2___}] := c[PR] DistributeChiralities[{rest1}, {rest2}];
 
@@ -25,22 +25,21 @@ DistributeChiralities[chiralities_List, couplings_List] :=
 MultLF[{factor_, func_, chiralities_List}, particles_List, expr_] := 
     factor func[Sequence @@ (
         If[#3 === 1, SARAH`Mass2[#1], 
-           SARAH`Mass2[#1, #2]] & @@@ particles
+           SARAH`Mass2[#1, #2]]& @@@ particles
     )] DistributeChiralities[chiralities, expr];
 
 MultiplyLoopFunction[loopfuncs_List, particles_List, expr_] := 
-    Total[MultLF[#, particles, expr] & /@ loopfuncs];
+    Total[MultLF[#, particles, expr]& /@ loopfuncs];
 
 (* check for ambiguous contraction of indices *)
 IsAmbiguousContraction[indices_List] := 
-    Or @@ ((#[[2]] > 2) & /@
+    Or @@ ((#[[2]] > 2)& /@
            Tally[First /@ (DeleteCases[indices, {_, SARAH`gE1 | SARAH`gE2}] /.
                            SARAH`bar -> Identity /. Susyno`LieGroups`conj -> Identity)]);
 
 (* one or more fields with indices appear more once *)
-
 IsAmbiguousIndex[indices_List] := 
-    Or @@ ((#[[2]] > 1) & /@ Tally[First /@ indices]);
+    Or @@ ((#[[2]] > 1)& /@ Tally[First /@ indices]);
 
 DistributeIndices[{}, coupling_] := coupling;
 DistributeIndices[indices_List, c : C[particles__]] :=
@@ -48,12 +47,13 @@ DistributeIndices[indices_List, c : C[particles__]] :=
        (* replace indices in order from indices list *)
        Assert[First /@ indices === {particles}];
        C[Sequence @@ 
-         MapThread[(#1 /. #2) &, {{particles}, (Rule[#1, AppendIndex[#1, #2]] & @@@ indices)}]]
+         MapThread[(#1 /. #2)&, {{particles}, (Rule[#1, AppendIndex[#1, #2]]& @@@ indices)}]]
        ,
        (* use one common replacement rule for all fields *)
-       c /. (Rule[#1, AppendIndex[#1, #2]] & @@@ indices)
+       c /. (Rule[#1, AppendIndex[#1, #2]]& @@@ indices)
     ];
 
+(* multiplies all given couplings *)
 MultiplyCouplings[lst_List] := DistributeIndices @@@ lst;
 
 SumOverIndices[{}, expr_] := expr;
@@ -62,12 +62,14 @@ SumOverIndices[{{_, _, 1}, rest___}, expr_] :=
 SumOverIndices[{{_, idx_, range_}, rest___}, expr_] :=
     SumOverIndices[{rest}, SARAH`sum[idx, 1, range, expr]];
 
+(* combines list of particles/loop functions/couplings to a sum *)
 CreateTadpoleDiag[{particles_List, loopFuncs_List, couplings_List}] :=
     SumOverIndices[particles, 
                    MultiplyLoopFunction[loopFuncs, particles, 
                                         MultiplyCouplings[couplings]]];
 
-SumTadpoleType[{type_, diags_}] := Total[CreateTadpoleDiag /@ diags];
+(* sums all diagrams that for a given type *)
+SumTadpoleType[{type_, diags_List}] := Total[CreateTadpoleDiag /@ diags];
 
 ConvertSarah2LDiagramList[tad_List, head_:Total] :=
     head[SumTadpoleType /@ tad] //. {
