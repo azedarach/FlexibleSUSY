6929b7dd654d1cdfe5f2773dbfd7031d6afc3cda
Alexander Voigt <Alexander.Voigt@physik.rwth-aachen.de>
2018-03-02 09:07:15 +0100

    correcting reading of file
    need to jump back to the beginning to start reading
    
    And print out B11 function.

--- test/test_pv2_run_collier.F90
+++ test/test_pv2_run_collier.F90
@@ -4,10 +4,10 @@ program run_loops
   Implicit none
 
   double precision :: p2, m12, m22, mu2
-  double complex :: A01,A02,B0,B1,B00
+  double complex :: A01,A02,B0,B1,B00,B11
   double complex, allocatable :: Bcoeff(:,:),Bcoeffuv(:,:)
   double precision, allocatable :: Berr(:)
-  integer :: i, rank
+  integer :: rank, st
   character(len=255) :: read_line, file_name
   logical :: file_exists=.False.
 
@@ -24,17 +24,26 @@ program run_loops
      write(*,*) "Error: file ",trim(file_name)," does not exist!"
      stop 1
   end if
-  open(100, file=trim(file_name), err=1)
+  open(100, file=trim(file_name), iostat=st)
+  if (st.ne.0) then
+     write(*,*) "Error: could not open file."
+     stop 1
+  end if
 
   call Init_cll(6,6,"")
 
   write (*,*) "# A0(m0^2) A0(m1^2) B0(p^2,m0^2,m1^2) B1(p^2,m0^2,m1^2) B00(p^2,m0^2,m1^2) B11(p^2,m0^2,m1^2)"
 
   do
-     read(100,"(a255)", end=1, err=1) read_line
+     read(100, "(a255)", iostat=st) read_line
+     if (st.ne.0) then
+        exit
+     end if
+
      if (read_line(1:1).eq."#") cycle
      if (read_line.eq."") cycle
 
+     backspace(100) ! jump to beginning of the line
      read(100,*) p2, m12, m22, mu2
 
      call SetMuUV2_cll(mu2)
@@ -47,10 +56,11 @@ program run_loops
      B0 = Bcoeff(0,0)
      B1 = Bcoeff(0,1)
      B00 = Bcoeff(1,0)
+     B11 = Bcoeff(1,1)
 
-     write(*,*) real(A01), real(A02), real(B0), real(B1), real(B00)
+     write(*,*) real(A01), real(A02), real(B0), real(B1), real(B00), real(B11)
   end do
 
-1 close(100)
+  close(100)
 
 end program run_loops
